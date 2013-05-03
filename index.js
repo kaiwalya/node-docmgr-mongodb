b9b85c1d5ebcc2d2db7199c4fc1b5b83535da170
Kaiwalya Kher <knkher@gmail.com>
2013-05-03 03:11:46 -0700

    Improvements.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var docmgr = require('docmgr');
 
 var PTDB = function (db, cb) {
 	var This = this;
@@ -7,24 +8,76 @@ var PTDB = function (db, cb) {
 
 var PTCollection = function (coll, pt, docType) {
 	var This = this;
+	This.coll = coll;
 	This.pt = pt;
+	This.docType = docType;
 };
 
-var PTCursor = function(ptc, q, projection, options, callback){
-	new PTCursor
-}
-
-PTCollection.prototype.find = function(q, projection, options, callback) {
+var PTCursor = function (cursor) {
 	var This = this;
-	This.coll.find();
-	new PTCursor(This, q, projection, options, callback);
-	return 
+	This.cursor = cursor;
 };
+
+PTCursor.prototype.next = function (callback) {
+	var This = this;
+	This.cursor.nextObject(function (err, obj) {
+		return callback(err, obj);
+	});
+	return;
+};
+
+PTCollection.prototype.insert = function (obj, options, callback) {
+	var This = this;
+	This.coll.insert(obj, {w: 1}, callback);
+};
+
+PTCollection.prototype.update = function (obj, doc, options, callback) {
+	var This = this;
+	var opt = {w: 1};
+	var uo = new docmgr.UpdateOptions(options);
+	if (!uo.isSingle()) {
+		opt.multi = true;
+	}
+	This.coll.update(obj, doc, opt, callback);
+};
+
+PTCollection.prototype.find = function (q, projection, options, callback) {
+	var This = this;
+	var outOptions = {};
+	if (projection) {
+		outOptions.fields = projection;
+	}
+	var fo = new docmgr.FindOptions(options);
+	This.coll.find(q, outOptions, function (err, result) {
+		if (err)
+			return callback(err);
+		if (fo.isSingle()) {
+			result.count(function (err, count) {
+				if (count === 0) {
+					return callback();
+				}
+				else if (count === 1) {
+					var cursor = new PTCursor(result);
+					return cursor.next(callback);
+				}
+				else {
+					callback(new docmgr.OutOfBoundsException());
+				}
+			});
+		}
+		else {
+			callback(err, new PTCursor(result));
+		}
+	});
+
+	return;
+};
+
 
 PTDB.prototype.collection = function (docType, callback) {
+	var This = this;
 	This.db.collection(docType, function (err, mColl) {
-		new PTCollection(mColl, This, docType);
-		return callback(err, This);
+		return callback(err, new PTCollection(mColl, This, docType));
 	});
 	return;
 };
